9a6f8e451fefaa08c8ad7025c16cd1a9078ee1d6
Kyle <capriok7@gmail.com>
2021-08-04 20:48:29 -0700

    cors

--- Server/src/server.ts
+++ Server/src/server.ts
@@ -1,7 +1,8 @@
+require('dotenv').config();
 import * as express from "express"
 import * as cors from 'cors'
 import * as swt from './swt.router'
-// import { corsOptions, corsMiddleware } from './cors/cors'
+import { corsOptions, corsMiddleware } from './cors/cors'
 
 const server = express()
 const port = process.env.PORT || 9000
@@ -9,8 +10,8 @@ const port = process.env.PORT || 9000
 server.use(express.json())
 server.use(cors())
 
-// const origins = ['http://raspberrypi:9000', 'http://localhost:3000']
-// server.use(cors(corsOptions(origins)), corsMiddleware)
+const origins = [process.env.APP_ORIGIN, 'http://localhost:3000']
+server.use(cors(corsOptions(origins)), corsMiddleware)
 
 server.use('/swt', swt.router)
 
