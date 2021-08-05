695363feda908c6a388a080ba0bcc9ee359b8329
Kyle <capriok7@gmail.com>
2021-08-04 20:06:45 -0700

    cors

--- Server/src/server.ts
+++ Server/src/server.ts
@@ -1,15 +1,15 @@
 import * as express from "express"
-// import * as cors from 'cors'
+import * as cors from 'cors'
 import * as swt from './swt.router'
 // import { corsOptions, corsMiddleware } from './cors/cors'
 
 const server = express()
 const port = process.env.PORT || 9000
-// const origins = ['http://raspberrypi:9000', 'http://localhost:3000']
 
 server.use(express.json())
-
 server.use(cors())
+
+// const origins = ['http://raspberrypi:9000', 'http://localhost:3000']
 // server.use(cors(corsOptions(origins)), corsMiddleware)
 
 server.use('/swt', swt.router)
