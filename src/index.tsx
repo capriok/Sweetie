a7ea0018f6930a320a7e9717cdb74fdfa70563a5
capriok <capriok7@gmail.com>
2021-11-11 11:58:32 -0700

    Update

--- src/index.tsx
+++ src/index.tsx
@@ -7,12 +7,17 @@ import Api from './api'
 import Splash from './Components/Common/Splash'
 import Suite from './Components/Suite/Suite'
 import Sweetie from './Components/Sweetie/Sweetie'
+import { startOfToday } from 'date-fns'
 
 function Index() {
   const [serverIdle, setServerIdle] = useState<boolean>(true)
   const { state, dispatch } = useDataFetch()
 
   useEffect(() => {
+    const ApplicaitonDate: Date = startOfToday()
+    ApplicaitonDate.setMinutes(ApplicaitonDate.getMinutes() - ApplicaitonDate.getTimezoneOffset())
+    console.log('ApplicaitonDate:', ApplicaitonDate.toJSON())
+
     if (process.env.NODE_ENV === 'development') return setServerIdle(false)
 
     Api.ServerPing().then(() => {
