61de660c3b86ae219e90c7207d81073e10c8c43f
Kyle Caprio <kyle@MacBook-Pro.local>
2021-12-10 14:25:00 -0800

    Crimas Logic Update

--- src/Components/Common/Splash.tsx
+++ src/Components/Common/Splash.tsx
@@ -5,9 +5,9 @@ import '../../Styles/Common/splash.scss'
 
 const Splash: React.FC<any> = () => {
 	let isProd = process.env.NODE_ENV === 'production'
-	let isSuite = window.innerWidth < 500
+	let isSweetie = window.location.pathname === '/sweetie'
 
-	const Splash_Orientation = isProd && !isSuite ? 'Splash-vert' : ''
+	const Splash_Orientation = isProd && isSweetie ? 'Splash-vert' : ''
 
 	return (
 		<div id="Splash">
