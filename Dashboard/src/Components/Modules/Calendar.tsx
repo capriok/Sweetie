132f206c3af6c9707dd3395b0549cad292c886aa
capriok <capriok7@gmail.com>
2022-03-21 18:45:42 -0700

    Update

--- Dashboard/src/Components/Modules/Calendar.tsx
+++ Dashboard/src/Components/Modules/Calendar.tsx
@@ -3,59 +3,59 @@ import React, { useEffect, useState } from 'react'
 import 'Styles/modules/calendar.scss'
 
 interface Props {
-	state: SwtState
+  state: SwtState
 }
 
 const CalendarModule: React.FC<Props> = (props) => {
-	const { state } = props
-	const [calendar, setCalendar] = useState<Array<CalendarDay>>([])
+  const { state } = props
+  const [calendar, setCalendar] = useState<Array<CalendarDay>>([])
 
-	useEffect(() => {
-		setCalendar(state.calendar)
-	}, [state.calendar])
+  useEffect(() => {
+    setCalendar(state.calendar)
+  }, [state.calendar])
 
-	function eventClassName(event: CalendarEvent) {
-		const cns = ['dot']
-		const work = 'Work'
+  function dotClassName(event: CalendarEvent) {
+    const cns = ['dot']
 
-		if (event.name.includes(work)) cns.push('work')
+    if (event.name.includes('Work')) cns.push('work')
+    if (event.name.includes('Payday')) cns.push('payday')
 
-		return cns.join(' ')
-	}
+    return cns.join(' ')
+  }
 
-	return (
-		<div className="calendar-module">
-			<div className="module-cont">
-				<div className="calendar-header">
-					{weekdays.map((weekday, i) => (
-						<div key={i} className="weekday">
-							<p>{weekday}</p>
-						</div>
-					))}
-				</div>
-				<div className="calendar-body">
-					{calendar.map((day, i) => (
-						<div key={i} className={day.classNames.day}>
-							<p className={day.classNames.number}>{day.number}</p>
-							<div className="events">
-								{day.events.map((event, i) =>
-									<div key={i} className={eventClassName(event)}>
-										•
-									</div>
-								)}
-							</div>
-						</div>
-					))}
-				</div>
-			</div>
-		</div>
-	)
+  return (
+    <div className="calendar-module">
+      <div className="module-cont">
+        <div className="calendar-header">
+          {weekdays.map((weekday, i) => (
+            <div key={i} className="weekday">
+              <p>{weekday}</p>
+            </div>
+          ))}
+        </div>
+        <div className="calendar-body">
+          {calendar.map((day, i) => (
+            <div key={i} className={day.classNames.day}>
+              <p className={day.classNames.number}>{day.number}</p>
+              <div className={day.classNames.number.includes('today') ? 'events' : 'dots'}>
+                {day.events.map((event, i) =>
+                  day.classNames.number.includes('today')
+                    ? <div key={i} className="today-event">
+                      {event.name}
+                    </div>
+                    : <div key={i} className={dotClassName(event)}>
+                      •
+                    </div>
+                )}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default CalendarModule
 
-const weekdays = [
-	'Sunday', 'Monday',
-	'Tueday', 'Wednesday',
-	'Thursday', 'Friday', 'Saturday'
-]
+const weekdays = ['Sunday', 'Monday', 'Tueday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
